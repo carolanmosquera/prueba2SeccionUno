8d622b0c9119d7afe7fd39405f27f595fba47423
carol prueba <carolanmosquera>
2024-08-02 10:53:43 -0500

    cinco

--- ui/Main.java
+++ ui/Main.java
@@ -1,7 +1,18 @@
 package ui;
 
 public class Main {
+
+    private int hell;
+        public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+            System.out.println( "Hello World!" );
+            System.out.println( "Hello World!" );
+        }
+
     //yoloooooooooooooooo
     //chanfle funciono
+    //HELP
+
 
 }
